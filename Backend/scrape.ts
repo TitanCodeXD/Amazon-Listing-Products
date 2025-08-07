0ac59232c3776924b3c54bc343ac1c255e1036e0
Wesley Santos <wesleysantos32892653@outlook.com>
2025-08-07 00:38:01 -0300

    Frontends styles improvement

--- Backend/scrape.ts
+++ Backend/scrape.ts
@@ -7,21 +7,22 @@ export async function scrapeAmazon(keyword: string) {
     const encodedKeyword = encodeURIComponent(keyword);
     const url = `https://www.amazon.com/s?k=${encodedKeyword}`;
 
-    const response = await axios.get(
-        `https://www.amazon.com.br/s?k=${encodeURIComponent(keyword)}`,
-        {
-            headers: {
-                'User-Agent':
-                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-                'Accept-Language': 'en-US,en;q=0.9',
-            },
-        }
-    );
+    const response = await axios.get(url, {
+        headers: {
+            'User-Agent':
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept-Language': 'en-US,en;q=0.9',
+        },
+    });
+
+    console.log(response.data.slice(0, 1000));
 
     const dom = new JSDOM(response.data);
     const document = dom.window.document;
 
-    const productDivs = [...document.querySelectorAll('div.s-main-slot > div')];
+    const productDivs = [
+        ...document.querySelectorAll('div[data-component-type="s-search-result"]'),
+    ];
 
     // Here i need to get the title, rating, reviews, and image of each product by querying the DOM
     // So i need to find the right selectors for each of these elements
